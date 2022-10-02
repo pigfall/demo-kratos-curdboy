4209d5da39a98aa0756ad63ddeaf14f2bd597fb8
pigfall <koi_town@outlook.com>
2022-10-02 23:23:20 +0900

    init

---
+++ cmd/demo-kratos-curdboy/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/pigfall/demo-kratos-curdboy/internal/biz"
+	"github.com/pigfall/demo-kratos-curdboy/internal/conf"
+	"github.com/pigfall/demo-kratos-curdboy/internal/data"
+	"github.com/pigfall/demo-kratos-curdboy/internal/server"
+	"github.com/pigfall/demo-kratos-curdboy/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
